548147abebf486f4e379b92038ff152f9c2106e9
Hoon Wee <mrgravity817@gmail.com>
2023-12-29 15:08:07 +0900

    feat: add money types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod financial;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod prelude {
+    pub use crate::financial::{iso, Money};
 }
