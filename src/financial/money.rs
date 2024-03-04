9c3dd1efa376248e7bb2d098c8a538120d9f2c6b
Hoon Wee <mrgravity817@gmail.com>
2024-03-04 14:55:54 +0900

    fix: use u64 for Money amount

--- src/financial/money.rs
+++ src/financial/money.rs
@@ -2,12 +2,11 @@
 ///
 /// ## Fields
 /// - `amount` - The amount of money.
-///    This is a floating point number to allow for fractional amounts.
 /// - `currency` - The currency of the money, represented as an ISO 4217 currency code.
 pub struct Money {
     amount: MoneyAmount,
     currency: Currency,
 }
 
-pub type MoneyAmount = f64;
+pub type MoneyAmount = u64;
 pub type Currency = String;
